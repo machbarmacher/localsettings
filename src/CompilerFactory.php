d53a7ffd03809910a961b06098605461445ab1de
Axel Rutz <axel.rutz@clever-systems.net>
2017-02-23 18:15:45 +0100

    Added and used a compiler factory

---
+++ src/CompilerFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace clever_systems\mmm_builder;
+
+
+class CompilerFactory {
+  /** @var string */
+  protected $include;
+  /** @var Project */
+  protected $project;
+  /** @var Compiler */
+  protected $compiler;
+
+  /**
+   * CompilerFactory constructor.
+   * @param string|null $include
+   */
+  public function __construct($include = NULL) {
+    if (!$include) {
+      $include = '../mmm-project.php';
+    }
+    $this->include = $include;
+    $this->project = @include $include;
+    if ($this->valid()) {
+      $this->compiler = new Compiler($this->project);
+    }
+  }
+
+
+  public function valid() {
+    return (bool) $this->project;
+  }
+
+  public function get() {
+    return $this->compiler;
+  }
+
+  public function validate() {
+    if (!$this->valid()) {
+      return drush_set_error('DRUSH_mmm_builder_ERROR', dt('File not found: @file', ['@file' => $this->include]));
+    }
+    return TRUE;
+  }
+
+}
