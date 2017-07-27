d1042083942fad49dc1bf544c4db01af40566704
Axel Rutz <axel.rutz@clever-systems.net>
2017-07-27 18:59:01 +0200

    Moved method

---
+++ src/CompileAliases.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace machbarmacher\localsettings;
+
+use machbarmacher\localsettings\RenderPhp\PhpFile;
+use machbarmacher\localsettings\RenderPhp\PhpRawStatement;
+use machbarmacher\localsettings\RenderPhp\PhpStatements;
+
+class CompileAliases {
+
+  /**
+   * @param \machbarmacher\localsettings\RenderPhp\PhpFile $php
+   * @param \machbarmacher\localsettings\InstallationInterface[] $installations
+   */
+  public static function addAliasAlterCode(PhpFile $php, $installations) {
+    $local_server_checks = [];
+    foreach ($installations as $installation_name => $installation) {
+      $check = $installation->getServer()->getLocalServerCheck("\$alias['remote-host']", "\$alias['remote-user']");
+      $local_server_checks[$check] = $check;
+    }
+    $local_server_check_statements = new PhpStatements();
+    foreach ($local_server_checks as $local_server_check) {
+      $local_server_check_statements->addStatement(new PhpRawStatement(
+        "  \$is_local = \$is_local || $local_server_check;"
+      ));
+    }
+
+    $php->addRawStatement(<<<EOD
+
+// Alter local aliases and add @this here as the drush alter hook is broken. 
+\$current_sites = [];
+foreach (\$aliases as \$alias_name => &\$alias) {
+  if (!isset(\$alias['remote-host']) || !isset(\$alias['remote-user'])) {
+    continue;
+  }
+  \$is_local = FALSE;
+$local_server_check_statements
+  if (\$is_local) {
+    unset(\$alias['remote-host']);
+    unset(\$alias['remote-user']);
+  }
+
+  \$is_current = \$is_local && realpath(\$alias['root']) == realpath(DRUSH_DRUPAL_CORE);
+  if (\$is_current) {
+    \$current_sites["@\$alias_name"] = \$alias;
+  }
+}
+if (count(\$current_sites) == 1) {
+  \$aliases['this'] = reset(\$current_sites);
+}
+else {
+  \$aliases['this'] = ['site-list' => array_keys(\$current_sites)];
+}
+
+EOD
+    );
+  }
+
+}
--- src/Compiler.php
+++ src/Compiler.php
@@ -62,7 +62,7 @@ class Compiler {
     foreach ($this->project->getInstallations() as $installation_name => $installation) {
       $installation->compileAliases($php);
     }
-    $this->addAliasAlterCode($php);
+    CompileAliases::addAliasAlterCode($php, $this->project->getInstallations());
     $commands->add(new WriteFile("../localsettings/aliases.drushrc.php", $php));
 
     $server_setting_files = [];
@@ -113,53 +113,6 @@ class Compiler {
     $php->addRawStatement(<<<EOD
 \$installation = {$settings_variable}['localsettings']['installation'] = '$installation_name';
 \$unique_site_name = {$settings_variable}['localsettings']['unique_site_name'] = "$unique_site_name";
-EOD
-    );
-  }
-
-  /**
-   * @param \machbarmacher\localsettings\RenderPhp\PhpFile $php
-   */
-  protected function addAliasAlterCode(PhpFile $php) {
-    $local_server_checks = [];
-    foreach ($this->project->getInstallations() as $installation_name => $installation) {
-      $check = $installation->getServer()->getLocalServerCheck("\$alias['remote-host']", "\$alias['remote-user']");
-      $local_server_checks[$check] = $check;
-    }
-    $local_server_check_statements = new PhpStatements();
-    foreach ($local_server_checks as $local_server_check) {
-      $local_server_check_statements->addStatement(new PhpRawStatement(
-        "  \$is_local = \$is_local || $local_server_check;"
-      ));
-    }
-
-    $php->addRawStatement(<<<EOD
-
-// Alter local aliases and add @this here as the drush alter hook is broken. 
-\$current_sites = [];
-foreach (\$aliases as \$alias_name => &\$alias) {
-  if (!isset(\$alias['remote-host']) || !isset(\$alias['remote-user'])) {
-    continue;
-  }
-  \$is_local = FALSE;
-$local_server_check_statements
-  if (\$is_local) {
-    unset(\$alias['remote-host']);
-    unset(\$alias['remote-user']);
-  }
-
-  \$is_current = \$is_local && realpath(\$alias['root']) == realpath(DRUSH_DRUPAL_CORE);
-  if (\$is_current) {
-    \$current_sites["@\$alias_name"] = \$alias;
-  }
-}
-if (count(\$current_sites) == 1) {
-  \$aliases['this'] = reset(\$current_sites);
-}
-else {
-  \$aliases['this'] = ['site-list' => array_keys(\$current_sites)];
-}
-
 EOD
     );
   }
