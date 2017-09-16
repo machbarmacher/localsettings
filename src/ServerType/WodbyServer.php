2bdd600e08698ba531b6e194a3fff40afa4491e3
Axel Rutz <axel.rutz@clever-systems.net>
2017-09-17 01:58:53 +0200

    Fixed gen syntax

--- src/ServerType/WodbyServer.php
+++ src/ServerType/WodbyServer.php
@@ -89,7 +89,7 @@ class WodbyServer extends ServerBase {
    * @return string
    */
   public function getRuntimeIsLocalCheck() {
-    return "preg_match('/\\.$this->app/u', (string)getenv('WODBY_APP_NAME')";
+    return "preg_match('/\\.$this->app/u', (string)getenv('WODBY_APP_NAME'))";
   }
 
 }
